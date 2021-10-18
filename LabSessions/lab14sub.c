69916548844f7244a29e82de1a71e47bf3b6a27a
kurianbenoy <kurian.bkk@gmail.com>
2021-10-18 17:32:23 +0530

    add weeks of lab deadlines

--- LabSessions/lab14sub.c
+++ LabSessions/lab14sub.c
@@ -7,95 +7,124 @@ struct Node {
     struct Node *next;
 };
 
+void insertAfter(struct Node **head_ref, struct Node *next_node, int new_data) {
+    if(next_node == NULL) {
+        printf("Not null");
+    }
+    
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    
+    new_node->data = new_data;
+    new_node->prev = next_node->prev;
+    next_node->prev = new_node;
+    new_node->next = next_node;
+    
+    if(new_node->prev != NULL) {
+        new_node->prev->next = new_node;
+    }
+    else {
+        (*head_ref) = new_node;
+    }
+    
+}
+
+void searchForElement(struct Node * head) {
+    int count = 0, num_insertions = 0;
+    while(head != NULL) {
+        if(head->data == 24) {
+            count += 1;
+            // pos_24 = head;
+        }
+        head = head +1;
+    }
+    
+    if(count>1) {
+        printf("%d occurrence of 24 found. Where should the insertion occur?", count);
+        scanf("%d", &num_insertions);
+    }
+    
+    else {
+        
+    }
+}
+
 int main() {
-    int count = 0,countspecific, data, occur, countspecificindex;
+    struct Node *head = NULL, *ptr, *prevptr;
+    int data, count = 0, countspecific=0, occur, countspecificindex=0;
+    int insert_element;
     int A[1000];
+    
     scanf("%d", &data);
     A[0] = data;
     
-    while(data != -999) {
-        if(data==42) {
-            countspecific++;
-            countspecificindex = count;
-        }
-        count ++;
-        scanf("%d", &data);
-        A[count] = data;
-    }
-
-    if(countspecific>1) {
-        printf("%d occurrence of 42 found. Where should deletion occur?", countspecific);
-        scanf("%d", &occur);
-
-        if(occur==1) {
-            printf("No element before 42");
-        }
-
-        for(int i=0;i<occur;i++) {
-            printf("%d", A[i]);
-        }
-        for(int i=occur+1;i<=count;i++) {
-            printf("%d", A[i]);
-        }
-    }
-
-    else {
-        for(int i=0;i<countspecificindex;i++) {
-            printf("%d ", A[i]);
-        }
-
-        for(int i=countspecificindex;i<=count;i++) {
-            printf("%d ", A[i]);
-        }
-    }
-}#include<stdio.h>
-#include<stdlib.h>
-
-struct Node {
-    int data;
-    struct Node *prev;
-    struct Node *next;
-};
-
-int main() {
-    int count = 0,countspecific, data, occur, countspecificindex;
-    int A[1000];
-    scanf("%d", &data);
-    A[0] = data;
     
     while(data != -999) {
-        if(data==42) {
+        // ptr = (struct Node *)malloc(sizeof(struct Node));
+        // ptr->data = data;
+        
+        // if(head == NULL) {
+        //     head = ptr;
+        //     head->prev = NULL;
+        // }
+        
+        // else {
+        //     prevptr->next = ptr;
+        //     ptr->prev = prevptr;
+        // }
+        // prevptr = ptr;
+        if(data == 24) {
             countspecific++;
             countspecificindex = count;
         }
-        count ++;
+        count++;
         scanf("%d", &data);
         A[count] = data;
     }
-
-    if(countspecific>1) {
-        printf("%d occurrence of 42 found. Where should deletion occur?", countspecific);
+    
+    scanf("%d", &insert_element);
+    
+    if(countspecific <1) {
+        printf("At least one element must be 24.\n");
+    }
+    
+    else if(countspecific>1) {
+        printf("%d occurrence of 24 found. Where should the insertion occur?\n", countspecific);
+        
         scanf("%d", &occur);
-
-        if(occur==1) {
-            printf("No element before 42");
+        
+        if(insert_element==24) {
+            for(int i=0;i<count;i++) {
+                printf("%d", A[i]);
+            }
         }
-
-        for(int i=0;i<occur;i++) {
-            printf("%d", A[i]);
+        
+        else {
+        
+        // for(int i=0;i<occur-1;i++) {
+        //     printf("%d ", A[i]);
+        // }
+        
+        for(int i=count;i>=occur;i--) {
+            A[i+1] = A[i];
         }
-        for(int i=occur+1;i<=count;i++) {
-            printf("%d", A[i]);
+        
+        A[occur] = insert_element;
+        
+        for(int i=0;i<=count;i++) {
+            printf("%d ", A[i]);
+        }
         }
     }
-
+    
     else {
-        for(int i=0;i<countspecificindex;i++) {
-            printf("%d ", A[i]);
+        for(int i=count;i>=0;i--) {
+            A[i+1] = A[i];
         }
-
-        for(int i=countspecificindex;i<=count;i++) {
+        
+        A[countspecificindex+1] = insert_element;
+        
+        for(int i=0;i<=count;i++) {
             printf("%d ", A[i]);
-        }
+    }
     }
 }
